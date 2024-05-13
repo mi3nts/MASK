47d822d61dcf36112bb313eade0ed0844a74c978
lakihaomal <lhw150030@utdallas.edu>
2024-05-13 10:35:42 -0500

    minor updates

--- firmware/xu4Mqtt/bno080BaseI2cCode.py
+++ firmware/xu4Mqtt/bno080BaseI2cCode.py
@@ -12,39 +12,7 @@
 # # Define the I2C bus number
 # bus_number = 4  # Adjust according to your setup
 
-# # Function to check for devices on the I2C bus
-# def detect_devices(bus):
-#     devices = []
-#     for address in range(128):  # I2C addresses range from 0x00 to 0x7F
-#         try:
-#             bus.read_byte(address)
-#             devices.append(address)
-#         except OSError:
-#             pass
-#     return devices
 
-# try:
-#     # Create an SMBus object for the specified bus number
-
-#     # Create an SMBus object with the specified speed
-#     bus = smbus2.SMBus(bus_number, 400000)
-    
-#     # Detect devices on the I2C bus
-#     detected_devices = detect_devices(bus)
-    
-#     if detected_devices:
-#         print("Devices found on the I2C bus:")
-#         for device in detected_devices:
-#             print("Device at address 0x{:02X}".format(device))
-#     else:
-#         print("No devices found on the I2C bus.")
-
-# except Exception as e:
-#     print("Error:", e)
-
-# finally:
-#     # Close the bus to release resources
-#     bus.close()
 
 
 import smbus2
@@ -63,6 +31,17 @@ reporting_frequency = 400  # reporting frequency in Hz
 # Initialize I2C bus
 bus = smbus2.SMBus(4, 400000)  # Assuming you're using /dev/i2c-4
 
+# Function to check for devices on the I2C bus
+def detect_devices(bus):
+    devices = []
+    for address in range(128):  # I2C addresses range from 0x00 to 0x7F
+        try:
+            bus.read_byte(address)
+            devices.append(address)
+        except OSError:
+            pass
+    return devices
+
 
 def get_product_id():
     # Define command to request product ID
@@ -78,27 +57,49 @@ def get_product_id():
         #     print("Error:", e)
     print("Written to Device")
     time.sleep(0.2)  # Wait for response
-    
-    # Read response
-    while True:
-        try:
-            response = bus.read_i2c_block_data(BNO_ADDRESS, 0, 25)
-            print(response)
-            if response[4] == 0xF8:  # Check for command response
-                break
-        except Exception as e:
-            # Print the error message if an exception occurs
-            print("Error:", e)
-        time.sleep(0.1)  # Wait for response
+
+    try:
+        # Create an SMBus object for the specified bus number
         
-    # Parse response and print product ID
-    reset_cause = response[5]
-    sw_major = response[6]
-    sw_minor = response[7]
-    print("Product ID response:")
-    print("Reset cause:", hex(reset_cause))
-    print("SW Major:", hex(sw_major))
-    print("SW Minor:", hex(sw_minor))
+        # Detect devices on the I2C bus
+        detected_devices = detect_devices(bus)
+        
+        if detected_devices:
+            print("Devices found on the I2C bus:")
+            for device in detected_devices:
+                print("Device at address 0x{:02X}".format(device))
+        else:
+            print("No devices found on the I2C bus.")
+
+    except Exception as e:
+        print("Error:", e)
+
+    finally:
+        # Close the bus to release resources
+        bus.close()
+
+
+
+    # Read response
+    # while True:
+    #     try:
+    #         response = bus.read_i2c_block_data(BNO_ADDRESS, 0, 25)
+    #         print(response)
+    #         if response[4] == 0xF8:  # Check for command response
+    #             break
+    #     except Exception as e:
+    #         # Print the error message if an exception occurs
+    #         print("Error:", e)
+    #     time.sleep(0.1)  # Wait for response
+        
+    # # Parse response and print product ID
+    # reset_cause = response[5]
+    # sw_major = response[6]
+    # sw_minor = response[7]
+    # print("Product ID response:")
+    # print("Reset cause:", hex(reset_cause))
+    # print("SW Major:", hex(sw_major))
+    # print("SW Minor:", hex(sw_minor))
 
 get_product_id()
 
