81006ffccdd4b23a993bc29b6383d61731f80feb
lakihaomal <lhw150030@utdallas.edu>
2024-05-13 10:57:25 -0500

    minor updates

--- firmware/xu4Mqtt/bno080BaseI2cCode.py
+++ firmware/xu4Mqtt/bno080BaseI2cCode.py
@@ -135,7 +135,7 @@ address = 0x4A  # Example device address (BNO080)
 block_size = 32  # Example block size for reading data
 
 # Read data from the device
-data = [bus.read_byte_data(address, register) for register in range(block_size)]
+data = [bus.read_word_data(address, register) for register in range(0, block_size, 2)]
 
 print("Data received:", data)
 
