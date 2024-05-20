797a52d2ebec4642d9da9c533bc4fa598f22b378
lakihaomal <lhw150030@utdallas.edu>
2024-05-20 10:36:48 -0500

    COZIR Checks

--- firmware/xu4Mqtt/cozIRReader.py
+++ firmware/xu4Mqtt/cozIRReader.py
@@ -1,5 +1,3 @@
-
-#
 import serial
 import datetime
 from mintsXU4 import mintsSensorReader as mSR
@@ -9,45 +7,41 @@ import re
 
 import sys
 
-dataFolderReference    = mD.dataFolderReference
-portIn                 = "/dev/ttyS0"
+dataFolderReference = mD.dataFolderReference
+portIn = "/dev/ttyS0"
 
 baudRate = 9600
 
-expectedAltitude       = 200;
+# Altitude in Richardson TX
+expectedAltitude = 200
+
 
 def main(portNum):
 
-    # print(altitude_compensation_string(expectedAltitude))
-
     ser = serial.Serial(
-    port= portIn,\
-    baudrate=baudRate,\
-	parity  =serial.PARITY_NONE,\
-	stopbits=serial.STOPBITS_ONE,\
-	bytesize=serial.EIGHTBITS,\
-    timeout=0)
+        port=portIn,
+        baudrate=baudRate,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=0
+    )
 
     print(" ")
     print("Connected to: " + ser.portstr)
     print(" ")
-    line = []
 
     print("Setting the sensor into polling mode")
     ser.write(str.encode('K 2\r\n'))
     time.sleep(1)
-    
+
     print("Reading CO2 Data Point")
     ser.write(str.encode('Z\r\n'))
     time.sleep(1)
-    
+
     print("Setting COZIR to emit all data")
     ser.write(str.encode('M 04166\r\n'))
     time.sleep(1)
-    
-    # print("Asking for Data")
-    # ser.write(str.encode('Q\r\n'))
-    # time.sleep(1)
 
     print("Setting the value of auto calibration")
     ser.write(str.encode('P 8 1\r\n'))
@@ -60,7 +54,7 @@ def main(portNum):
     time.sleep(1)
 
     print("Setting Compensation Value")
-    ser.write(str.encode(altitude_compensation_string(expectedAltitude)))
+    ser.write(str.encode(generate_altitude_compensation_string(expectedAltitude)))
     time.sleep(1)
 
     print("Reading the altitude compensation value")
@@ -74,33 +68,33 @@ def main(portNum):
     while True:
         try:
             for c in ser.read():
+                line = []
                 line.append(chr(c))
                 if chr(c) == '\n':
                     print("-------------------------------------------------------------")
-                    # print(datetime.datetime.now())                     
-                    dataStringPost     = (''.join(line)).replace("\n","").replace("\r","").replace(" ","")
-                    # print(dataStringPost)
+                    dataStringPost = (''.join(line)).replace("\n", "").replace("\r", "").replace(" ", "")
                     time.sleep(1)
                     if check_format(dataStringPost):
                         mSR.COZIRAEH2000Write((decode_cozir_data(dataStringPost)))
                         ser.write(str.encode('Q\r\n'))
 
-                     
                     line = []
-        except:
-            print("Incomplete read. Something may be wrong with {0}".format(portIn))
+        except Exception as e:
+            print(f"Incomplete read. Something may be wrong with {portIn}: {e}")
             line = []
+
 
 def check_format(s):
     """
     Check if the string has the format 'H ddddd T ddddd Z ddddd z ddddd'.
-    
+
     :param s: The string to check.
     :return: True if the string matches the format, False otherwise.
     """
     pattern = r'^H\d{5}T\d{5}Z\d{5}z\d{5}$'
-    match   = re.match(pattern, s)
+    match = re.match(pattern, s)
     return bool(match)
+
 
 def decode_cozir_data(data):
     """
@@ -109,23 +103,24 @@ def decode_cozir_data(data):
     :return: A dictionary with decoded values.
     """
     print(data)
-    try:     
-        dateTime  = datetime.datetime.now()
-        humidity      = int(data[1:6]) / 10.0             # Assuming the humidity is given in tenths of percentage
-        temperature   = (int(data[7:12]) - 1000) / 10.0   # Assuming the temperature is given in tenths of degrees Celsius
-        co2Filtured   = int(data[13:18])                  # CO2 concentration in ppm
-        co2Recent     = int(data[19:])                    # Another CO2 concentration in ppm or another parameter
-        return [dateTime,co2Recent,co2Filtured,temperature,humidity]
-    
+    try:
+        dateTime = datetime.datetime.now()
+        humidity = int(data[1:6]) / 10.0  # Assuming the humidity is given in tenths of percentage
+        temperature = (int(data[7:12]) - 1000) / 10.0  # Assuming the temperature is given in tenths of degrees Celsius
+        co2Filtered = int(data[13:18])  # CO2 concentration in ppm
+        co2Recent = int(data[19:])  # Another CO2 concentration in ppm or another parameter
+        return [dateTime, co2Recent, co2Filtered, temperature, humidity]
+
     except (IndexError, ValueError) as e:
         print(f"Error decoding data: {e}")
         return None
 
-def altitude_compensation_string(altitude):
+
+def generate_altitude_compensation_string(altitude):
     cv = compensation_value(altitude)
     print("Compensation Value: ")
     print(cv)
-    setString  = "S "+ str(round(cv)) + "\r\n"
+    setString = "S " + str(round(cv)) + "\r\n"
     return setString
 
 
@@ -142,11 +137,11 @@ def compensation_value(altitude_m):
     base_value = 8192  # Base compensation value
     coefficient = 1.35  # Coefficient derived from the sea level difference formula
     return base_value + coefficient * altitude_m
-    
+
 
 if __name__ == "__main__":
     print("=============")
     print("    MINTS    ")
     print("=============")
-    print("Monitoring COZIR Sensor on port: {0}".format(portIn)+ " with baudrate " + str(baudRate))
+    print("Monitoring COZIR Sensor on port: {0}".format(portIn) + " with baudrate " + str(baudRate))
     main(portIn)
--- firmware/xu4Mqtt/gpsReader.py
+++ firmware/xu4Mqtt/gpsReader.py
@@ -7,54 +7,55 @@ from adafruit_extended_bus import ExtendedI2C as I2C
 from mintsXU4 import mintsSensorReader as mSR
 from mintsXU4 import mintsDefinitions as mD
 
-
-
-
 def main():
-
-
-    delta  = 1
+    delta = 1
+    resetDelta = 300
     lastGPRMC = time.time()
     lastGPGGA = time.time()
 
-    try:  
-    # Detecting if the GPS is Connected
-        i2c = I2C(6)
-        gps = adafruit_gps.GPS_GtopI2C(i2c, debug=False) # Use I2C interface
-        print("GPS found")
-    except Exception as e:
-        time.sleep(1)
-        print("No GPS found")
-        print ("Error and type: %s - %s." % (e,type(e)))
-        quit()
-
-    # Turn on everything (not all of it is parsed!)
-    print("Sending GPS Command")
-    gps.send_command(b"PMTK314,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0")
-    time.sleep(1)
-    print("Changing Update Frequency")
-    gps.send_command(b"PMTK220,1000")
-
-
     while True:
-        time.sleep(1)
         try:  
-            if not gps.update() or not gps.has_fix:
-                print("No Coordinates found")
-                print(gps.nmea_sentence) 
-                continue
-            dateTime  = datetime.datetime.now()
-            dataString = gps.nmea_sentence
-            if (dataString.startswith("$GPGGA") or dataString.startswith("$GNGGA")) and mSR.getDeltaTime(lastGPGGA,delta):
-                mSR.GPSGPGGA2Write(dataString,dateTime)
-                lastGPGGA = time.time()
-            if (dataString.startswith("$GPRMC") or dataString.startswith("$GNRMC")) and mSR.getDeltaTime(lastGPRMC,delta):
-                mSR.GPSGPRMC2Write(dataString,dateTime)
-                lastGPRMC = time.time()
+            # Detecting if the GPS is Connected
+            i2c = I2C(6)
+            gps = adafruit_gps.GPS_GtopI2C(i2c, debug=False) # Use I2C interface
+            print("GPS found")
+
+            # Turn on everything (not all of it is parsed!)
+            print("Sending GPS Command")
+            gps.send_command(b"PMTK314,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0")
+            time.sleep(1)
+            print("Changing Update Frequency")
+            gps.send_command(b"PMTK220,1000")
+
+            while True:
+                time.sleep(1)
+                if not gps.update() or not gps.has_fix:
+                    print("No Coordinates found")
+                    print(gps.nmea_sentence) 
+                    continue
+                
+                dateTime = datetime.datetime.now()
+                dataString = gps.nmea_sentence
+                if (dataString.startswith("$GPGGA") or dataString.startswith("$GNGGA")) and mSR.getDeltaTime(lastGPGGA, delta):
+                    mSR.GPSGPGGA2Write(dataString, dateTime)
+                    lastGPGGA = time.time()
+
+                if (dataString.startswith("$GPRMC") or dataString.startswith("$GNRMC")) and mSR.getDeltaTime(lastGPRMC, delta):
+                    mSR.GPSGPRMC2Write(dataString, dateTime)
+                    lastGPRMC = time.time()
+                
+                time.sleep(1)
+                if mSR.getDeltaTime(lastGPGGA, resetDelta):
+                    # Restarting the process
+                    print("Resetting GPS")
+                    time.sleep(10)
+                    break
+                
         except Exception as e:
-            time.sleep(.5)
+            time.sleep(1)
             print("GPS Error")
-            print ("Error and type: %s - %s." % (e,type(e)))
+            print("Error and type: %s - %s." % (e, type(e)))
+
 
 if __name__ == "__main__":
     print("=============")
