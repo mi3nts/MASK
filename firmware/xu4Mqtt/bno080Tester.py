d1df78c0652c5d9b687da4f7364600aac72c13d3
lakihaomal <lhw150030@utdallas.edu>
2024-05-20 18:50:33 -0500

    run all checks

--- firmware/xu4Mqtt/bno080Tester.py
+++ firmware/xu4Mqtt/bno080Tester.py
@@ -94,10 +94,12 @@ from adafruit_bno08x import (
     BNO_REPORT_MAGNETOMETER,
     BNO_REPORT_ROTATION_VECTOR,
 )
+
+from adafruit_extended_bus import ExtendedI2C as I2C
 from adafruit_bno08x.i2c import BNO08X_I2C
 
-i2c = busio.I2C(board.SCL, board.SDA, frequency=400000)
-bno = BNO08X_I2C(i2c)
+i2c     = I2C(4)
+bno     = BNO08X_I2C(i2c)
 
 bno.enable_feature(BNO_REPORT_ACCELEROMETER)
 bno.enable_feature(BNO_REPORT_GYROSCOPE)
