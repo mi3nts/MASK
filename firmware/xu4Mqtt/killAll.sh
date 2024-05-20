a2d806f191ecc18402908873ed5eea5bc603caf3
lakihaomal <lhw150030@utdallas.edu>
2024-05-20 11:34:38 -0500

    run all checks

--- firmware/xu4Mqtt/killAll.sh
+++ firmware/xu4Mqtt/killAll.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 #
 sleep 1
-kill $(pgrep -f 'ips7100ReaderV1.py')
-sleep 2
+kill $(pgrep -f 'ips7100Reader.py')
+sleep 1
 
-kill $(pgrep -f 'i2cReader.py')
-sleep 2
+kill $(pgrep -f 'climateReader.py')
+sleep 1
+
+kill $(pgrep -f 'bno080Reader.py')
+sleep 1
+
+kill $(pgrep -f 'cozIRReader.py')
+sleep 1
 
 kill $(pgrep -f 'gpsReader.py')
-sleep 2
+sleep 1
 
 kill $(pgrep -f 'batteryReader.py')
-sleep 2
+sleep 1
+
+python3 ipReader.py
+sleep 5
