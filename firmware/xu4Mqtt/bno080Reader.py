9db8c5f19c175665b2d0c7ce5b97ed920377c181
lakihaomal <lhw150030@utdallas.edu>
2024-05-21 14:26:15 -0500

    run all checks

--- firmware/xu4Mqtt/bno080Reader.py
+++ firmware/xu4Mqtt/bno080Reader.py
@@ -17,7 +17,7 @@ from adafruit_extended_bus import ExtendedI2C as I2C
 # bno     = BNO08X_I2C(i2c)
 
 debug        = False 
-bus          = I2C(4,frequency=400000)
+bus          = I2C(4)
 bno080       = BNO080(bus,debug)
 initTrials   = 5
 loopInterval = 2
