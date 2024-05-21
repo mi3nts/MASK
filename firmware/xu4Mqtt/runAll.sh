aa4b1e69def03071dd43afa39e5cd946be6a5705
lakithaomal <lhw150030@utdallas.edu>
2024-05-21 00:51:19 +0000

    minor updates

--- firmware/xu4Mqtt/runAll.sh
+++ firmware/xu4Mqtt/runAll.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-sleep 6
+sleep 60
 kill $(pgrep -f 'ips7100Reader.py')
 sleep 5
 python3 ips7100Reader.py &
@@ -11,15 +11,15 @@ sleep 5
 python3 climateReader.py &
 sleep 5
 
-kill $(pgrep -f 'bno080Reader.py')
-sleep 5
-python3 bno080Reader.py &
-sleep 5
+#kill $(pgrep -f 'bno080Reader.py')
+#sleep 5
+#python3 bno080Reader.py &
+#sleep 5
 
-#kill $(pgrep -f 'cozIRReader.py')
-#sleep 5
-#python3 cozIRReader.py &
-#sleep 5
+kill $(pgrep -f 'cozIRReader.py')
+sleep 5
+python3 cozIRReader.py &
+sleep 5
 
 kill $(pgrep -f 'gpsReader.py')
 sleep 5
