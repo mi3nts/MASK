28df8babb25692dc8207301fe3df8881755334cf
lakithaomal <lhw150030@utdallas.edu>
2024-10-01 15:44:09 +0000

    minor updates

--- firmware/xu4Mqtt/bno080Tester.py
+++ firmware/xu4Mqtt/bno080Tester.py
@@ -1,35 +1,64 @@
-# SPDX-FileCopyrightText: 2020 Bryan Siepert, written for Adafruit Industries
-#
-# SPDX-License-Identifier: Unlicense
-from time import sleep
+import time
+import datetime
 import board
 import busio
-from digitalio import DigitalInOut, Direction
-from adafruit_bno08x.spi import BNO08X_SPI
+from i2cMints.i2c_bno080 import BNO080
+from mintsXU4 import mintsSensorReader as mSR
+import os
+import sys
+from adafruit_bno08x.i2c import BNO08X_I2C
+import subprocess
+from adafruit_extended_bus import ExtendedI2C as I2C
 
-# need to limit clock to 3Mhz
-spi = busio.SPI(board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+from adafruit_bno08x import (
+    BNO_REPORT_ACCELEROMETER,
+    BNO_REPORT_GYROSCOPE,
+    BNO_REPORT_MAGNETOMETER,
+    BNO_REPORT_ROTATION_VECTOR,
+)
 
-cs = DigitalInOut(board.D12)
-cs.direction = Direction.OUTPUT
 
-int_pin = DigitalInOut(board.D13)
-int_pin.direction = Direction.INPUT
+debug        = False 
+bus          = I2C(4)
 
-wake_pin = DigitalInOut(board.D4)
-wake_pin.direction = Direction.INPUT
+#time.sleep(1)
+#print(bus.try_lock())
 
-reset_pin = DigitalInOut(board.D4)
-reset_pin.direction = Direction.INPUT
+#time.sleep(1)
+#print(bus.scan())
 
-bno = BNO08X_SPI(spi, cs, int_pin, wake_pin, reset_pin, debug=True)
+#time.sleep(1)
+#print(bus.unlock())
+
+
+time.sleep(1)
+bno       =  BNO08X_I2C(bus)
+
+bno.enable_feature(BNO_REPORT_ACCELEROMETER)
+bno.enable_feature(BNO_REPORT_GYROSCOPE)
+bno.enable_feature(BNO_REPORT_MAGNETOMETER)
+bno.enable_feature(BNO_REPORT_ROTATION_VECTOR)
 
 while True:
-    print("getting quat")
-    quat = bno.quaternion  # pylint:disable=no-member
+    time.sleep(0.5)
+    print("Acceleration:")
+    accel_x, accel_y, accel_z = bno.acceleration  # pylint:disable=no-member
+    print("X: %0.6f  Y: %0.6f Z: %0.6f  m/s^2" % (accel_x, accel_y, accel_z))
+    print("")
+
+    print("Gyro:")
+    gyro_x, gyro_y, gyro_z = bno.gyro  # pylint:disable=no-member
+    print("X: %0.6f  Y: %0.6f Z: %0.6f rads/s" % (gyro_x, gyro_y, gyro_z))
+    print("")
+
+    print("Magnetometer:")
+    mag_x, mag_y, mag_z = bno.magnetic  # pylint:disable=no-member
+    print("X: %0.6f  Y: %0.6f Z: %0.6f uT" % (mag_x, mag_y, mag_z))
+    print("")
+
     print("Rotation Vector Quaternion:")
+    quat_i, quat_j, quat_k, quat_real = bno.quaternion  # pylint:disable=no-member
     print(
-        "I: %0.6f  J: %0.6f K: %0.6f  Real: %0.6f" % (quat.i, quat.j, quat.k, quat.real)
+        "I: %0.6f  J: %0.6f K: %0.6f  Real: %0.6f" % (quat_i, quat_j, quat_k, quat_real)
     )
     print("")
-    sleep(0.5)
