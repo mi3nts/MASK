1e82b65f943bd1cf184be900c5ba43f4f402a40d
lakihaomal <lhw150030@utdallas.edu>
2024-10-02 18:15:32 -0500

    BNOO UPdates

--- firmware/xu4Mqtt/i2cMints/i2c_bno080.py
+++ firmware/xu4Mqtt/i2cMints/i2c_bno080.py
@@ -82,7 +82,6 @@ class BNO080:
 
         except Exception as e:
             time.sleep(5)
-            self.reset()
             print("An exception occurred:", type(e).__name__, "–", e) 
             time.sleep(5)
             print("BNO080 Not Found")
