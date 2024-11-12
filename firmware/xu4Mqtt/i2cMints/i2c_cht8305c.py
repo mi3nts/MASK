7f2ddd472d2708955f70926ec495df24ab9b3562
lakihaomal <lhw150030@utdallas.edu>
2024-11-11 20:04:02 -0600

    CHT8305 update

--- firmware/xu4Mqtt/i2cMints/i2c_cht8305c.py
+++ firmware/xu4Mqtt/i2cMints/i2c_cht8305c.py
@@ -5,38 +5,73 @@ import smbus2
 import struct
 import time
 
-CHT8305C_I2C_ADDR = 0x40
+CHT8305C_I2C_ADDR          = 0x40
+CHT8305C_REG_MANUFACTURER   = 0xFE
+CHT8305C_REG_VERSION        = 0xFF
 
 class CHT8305C:
-
     def __init__(self, i2c_dev,debugIn):
-        
         self.i2c_addr  = CHT8305C_I2C_ADDR
         self.i2c       = i2c_dev
         self.debug     = debugIn
 
-    def initiate(self,retriesIn):
+    def initiate(self):
         print("============== CHT8305C I2C ==============")
-        return True
+        if self.is_connected():
+            print("CHT8305C sensor connected")
+            time.sleep(1)
+            print(f"Manufacturer: {self.get_manufacturer()}")
+            time.sleep(1)
+            print(f"Version ID: {self.get_version_id()}")
+            return True
+        else:
+            print("CHT8305C sensor connection failed")
+            return False
+
+    def is_connected(self):
+        try:
+            self.i2c.write_quick(self.i2c_addr)
+            return True
+        except IOError:
+            return False
 
     def read_i2c(self, command, reply_size):
         # Command is  the register requested
         received_bytes = []
-        received_bytes.clear()
-
-        # Send command to the I2C device
-        self.i2c.write_byte(
-            CHT8305C_I2C_ADDR, command)
-        
-        # Delay for the I2C response
-        time.sleep(0.01)  
-        
-        # Delay for the I2C response
-        received_bytes = self.i2c.read_i2c_block_data(
-            CHT8305C_I2C_ADDR, command, reply_size)
-
-        time.sleep(0.01)
+        try:
+            # Send command to the I2C device
+            self.i2c.write_byte(
+                CHT8305C_I2C_ADDR, command)
+            # Delay for the I2C response
+            time.sleep(0.01)  
+    
+            # Delay for the I2C response
+            received_bytes = self.i2c.read_i2c_block_data(
+                CHT8305C_I2C_ADDR, command, reply_size)
+            
+        except Exception as e:
+            time.sleep(0.01)
+            print(f"Error reading I2C: {e}")
         return received_bytes;
+
+    def get_manufacturer(self):
+        manufacturer_data = self.read_i2c(CHT8305C_REG_MANUFACTURER, 2)
+        if manufacturer_data is None:
+            print("Failed to read manufacturer ID from sensor.")
+            return None
+        manufacturer_id = (manufacturer_data[0] << 8) | manufacturer_data[1]
+        return manufacturer_id
+
+    def get_version_id(self):
+        version_data = self.read_i2c(CHT8305C_REG_VERSION, 2)
+        if version_data is None:
+            print("Failed to read version ID.")
+            return None
+        # Convert the byte data into a 16-bit integer
+        version_id = (version_data[0] << 8) | version_data[1]
+        return version_id
+
+
 
     def read(self):
         # Read PC data
